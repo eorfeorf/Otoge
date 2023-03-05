bc9034f3c1dfb5a76b0d2d9d7f2a40b92aa5311e
EF <eorf256@gmail.com>
2023-03-05 13:13:00 +0900

    レイヤー分け

---
+++ Assets/Otoge/Scripts/InGame/Application/Interface/IDataContainer.cs
@@ -0,0 +1,9 @@
+namespace Otoge.Scripts.InGame.Application
+{
+    /// <summary>
+    /// データを扱うインターフェイス.
+    /// </summary>
+    public interface IDataContainer
+    {
+    }
+}
---
+++ Assets/Otoge/Scripts/InGame/Application/Interface/IPresenter.cs
@@ -0,0 +1,9 @@
+namespace Otoge.Scripts.InGame.Application
+{
+    /// <summary>
+    /// 描画を扱うインターフェイス.
+    /// </summary>
+    public interface IPresenter
+    {
+    }
+}
