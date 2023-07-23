925bfca1a1d05294f8fe341133ea49d790690408
EF <eorf256@gmail.com>
2023-07-23 11:32:33 +0900

    コメント修正

--- Assets/Otoge/Scripts/Runtime/Application/InGameLifetimeScope.cs
+++ Assets/Otoge/Scripts/Runtime/Application/InGameLifetimeScope.cs
@@ -12,27 +12,26 @@ namespace Otoge.Application
             
         protected override void Configure(IContainerBuilder builder)
         {
-            //
+            ////////////////////////////
             // Domain.
-            //
+            ////////////////////////////
             builder.Register<LifeCycle>(Lifetime.Singleton);
             builder.Register<InGamePlayer>(Lifetime.Singleton);
             builder.Register<ProgressTimer>(Lifetime.Singleton);
             builder.Register<BarLine>(Lifetime.Singleton);
             builder.Register<InputCommand>(Lifetime.Singleton);
             builder.Register<NoteContainer>(Lifetime.Singleton);
-            //builder.Register<InGameMainLoop>(Lifetime.Singleton);
             builder.Register<InGameConfiguration>(Lifetime.Singleton);
             
             builder.RegisterEntryPoint<InGameMainLoop>();
             
-            //
+            
+            ////////////////////////////
             // Presentation.
-            //
+            ////////////////////////////
             builder.Register<IInputEvent, InputEventPlayerPC>(Lifetime.Singleton);
             builder.RegisterComponent(noteViewFactory);
             builder.Register<NoteViewRepository>(Lifetime.Singleton);
-            //builder.Register<NotePresenter>(Lifetime.Singleton);
             builder.Register<InGameViewInfo>(Lifetime.Singleton);
 
             builder.RegisterEntryPoint<NotePresenter>();
